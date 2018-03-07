620af82c9b7238daeb9d3e75150c446431b0c552
Brendon Kuleck <bkuleck@caxy.com>
2018-03-07 10:47:32 -0600

    Services

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function(){
-  
+myApp.controller('mainController', function($scope, $log, $filter){
+  	
+	$scope.name = 'John';
+	$scope.formattedname = $filter('uppercase')($scope.name);
+	$log.info($scope.name);
+	$log.info($scope.formattedname);
+	
 });
