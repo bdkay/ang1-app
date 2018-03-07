0fb0618659d5405306b5981fec1c44682c1e0745
Brendon Kuleck <bkuleck@caxy.com>
2018-03-07 15:02:59 -0600

    Watchers and the Digest Loop

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
 
 	$scope.handle = '';
 	$scope.lowercasehandle = function(){
+		
 		return $filter('lowercase')($scope.handle);	
+	
 	};
+	$scope.$watch('handle', function(newValue, oldValue){
+		
+		console.info('Changed!');
+		console.log('Old: ' + oldValue);
+		console.log('New: ' + newValue);
+		
+	});
+	
+	setTimeout(function(){
+		$scope.$apply(function(){
+			$scope.handle = 'newtwitterhandle';
+			console.log('Scope changed!');	
+		})
+	}, 3000);
 	
 }]);
