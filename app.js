e3357c72391b6a46373baa9c1c2f7abf87550e6e
Brendon Kuleck <bkuleck@caxy.com>
2018-03-08 15:01:56 -0600

    Using restrict in custom directive

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ myApp.controller('thirdController', ['$scope', '$log', function($scope, $log){
 
 myApp.directive("searchResult", function(){
 	return {
-		restrict: 'AE',
+		restrict: 'AEC',
 		template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Steve Doe</h4><p class="list-grou-item-text">555 Main St., New York, NY 00100</p></a>',
 		replace: true
 	}
