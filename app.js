a7f12f19098816548887cb0d3ed4965d47560f33
Brendon Kuleck <bkuleck@caxy.com>
2018-03-08 15:10:10 -0600

    Custom directives and restrict

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ myApp.controller('thirdController', ['$scope', '$log', function($scope, $log){
 myApp.directive("searchResult", function(){
 	return {
 		restrict: 'AEC',
-		template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Steve Doe</h4><p class="list-grou-item-text">555 Main St., New York, NY 00100</p></a>',
+		templateUrl: 'directives/searchresult.html',
 		replace: true
 	}
 });
